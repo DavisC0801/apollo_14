31f8485a983a657a1e11cbea1060fc6eda7ede5b
davisc0801 <davisc1014@gmail.com>
2019-05-10 09:54:01 -0600

    Update view and model to include total time in space

--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -3,4 +3,8 @@ class Astronaut < ApplicationRecord
   has_many :missions, through: :astronaut_missions
 
   validates_presence_of :name, :age, :job
+
+  def total_space_time
+    missions.sum(:time_in_space)
+  end
 end
